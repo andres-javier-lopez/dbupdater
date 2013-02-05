9e529edc442c401577e537c0c5097d0a1b107082
Andrés Javier López <ajavier.lopez@gmail.com>
2013-02-05 17:04:56 -0600

    Initial commit

---
+++ install.php
@@ -0,0 +1,17 @@
+<?php
+// Author: Andrés Javier López <ajavier.lopez@gmail.com>
+
+require 'conexion.php';
+
+$command = "mysql --host=$host --user=$user --password=$password $database < baseline.sql";
+system($command);
+
+$mysql = new mysqli($host, $user, $password, $database);
+$result = $mysql->real_query('INSERT INTO `changelog` VALUES(NULL,1,0,0,"initial install", NOW())');
+
+if($result){
+	echo 'Success';
+}
+else {
+	echo 'Fail: '.$mysql->error;
+}
